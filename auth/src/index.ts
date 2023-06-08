42264a97956cb9d8bd3703870d8c824bbdf8b997
Rohit Goswami <rohitgoswamigi@gmail.com>
2023-06-08 13:14:19 +0530

    some changes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import { app } from "./app";
 const start = async () => {
-    console.log("OKOKok");
+    console.log("OKOKokOK");
     if (!process.env.JWT_KEY) {
         throw new Error("JWT_KEY must be defined");
     }
